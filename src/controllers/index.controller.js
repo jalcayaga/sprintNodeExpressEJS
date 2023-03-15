a10b7bdf72245a0de0ac170e43b95522abff0af9
Javier Alcayaga <javier.alcayaga@subred.cl>
2023-03-15 19:29:09 -0300

    funcionando, faltan algunas validaciones y los reportes

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -16,26 +16,17 @@ let puntajes = JSON.parse(json_puntajes);
 
 // render de rutas
 export const renderIndexPage = (req, res) =>
-  res.render("index", { resultados, equipos });
+  res.render("index", { resultados, equipos, carreras });
 /* ----------------------------- pagina reportes ---------------------------- */
 export const renderReportesPage = (req, res) => res.render("reportes", config);
 
 export const renderEditarNuevoResultado = (req, res) => {
-  const idPiloto = req.body.idPiloto;
-  const equipo = equipos.find((equipo) => equipo.idPiloto === idPiloto);
-  
- 
-  if (equipo) {
-    escuderia = equipo.escuderia;
-    piloto = equipo.piloto;
-  }
-  
-  console.log('equipos:', equipos);
-  console.log('carreras:', carreras);
-  console.log('puntajes:', puntajes);
-
-  res.render('nuevo-resultado', { equipos, carreras, puntajes, escuderia: 'Ferrari', piloto: 'Carlos Sainz' });
-
+  res.render("nuevo-resultado", {
+    equipos,
+    carreras,
+    puntajes,
+    resultados,
+  });
 };
 
 export const renderNuevoResultadoPage = (req, res) =>
@@ -44,26 +35,40 @@ export const renderNuevoResultadoPage = (req, res) =>
 
 /* --------------- // Editar o Crear objeto dentro de resultados.json --------------- */
 export const editarNuevoResultado = (req, res) => {
+  let { idPiloto, circuito, minutos, posicion, tecnicos, personales } =
+    req.body;
   // Leer archivo de resultados
   const json_resultados = fs.readFileSync("src/resultados.json", "utf-8");
   let resultados = JSON.parse(json_resultados);
+
   // Leer archivo de puntajes
-  const json_puntajes = fs.readFileSync("src/json/puntajes.json", "utf-8");
-  const puntajes = JSON.parse(json_puntajes);
+  const json_puntajes_path = "src/json/puntajes.json";
+  const json_puntajes = fs.readFileSync(json_puntajes_path, "utf-8");
+  if (!json_puntajes) {
+    console.log(`No se pudo leer el archivo ${json_puntajes_path}`);
+    return;
+  }
+  let puntajes = JSON.parse(json_puntajes);
+  // Leer archivo de equipos
+  const json_equipos_path = "src/json/equipos.json";
+  const json_equipos = fs.readFileSync(json_equipos_path, "utf-8");
+  if (!json_equipos) {
+    console.log(`No se pudo leer el archivo ${json_equipos_path}`);
+    return;
+  }
+  let equipos = JSON.parse(json_equipos);
 
+  /* --------------------------- algunos console.log -------------------------- */
+  console.log(equipos);
+  console.log(typeof puntajes); // "object"
+  console.log(puntajes);
   console.log(req.body);
-  let {
-    idPiloto,
-    escuderia,
-    nombre,
-    circuito,
-    minutos,
-    posicion,
-    tecnicos,
-    personales,
-  } = req.body;
 
-  if (!idPiloto || !circuito || !posicion) {
+  // Convertir a booleanos
+  tecnicos = tecnicos ? true : false;
+  personales = personales ? true : false;
+
+  if (!idPiloto || !minutos || !posicion) {
     res.status(400).send("Faltan datos obligatorios");
     return;
   }
@@ -73,7 +78,21 @@ export const editarNuevoResultado = (req, res) => {
     return;
   }
 
-  let piloto = resultados.find((piloto) => piloto.idPiloto === idPiloto);
+  /* ---------------------- Variables nombre y escuderia ---------------------- */
+  let nombre = "";
+  let escuderia = "";
+
+  // Buscar el equipo del piloto
+  let equipoPiloto = equipos.find((equipo) => equipo.idPiloto == idPiloto);
+
+  if (equipoPiloto) {
+    nombre = equipoPiloto.nombre;
+    escuderia = equipoPiloto.escuderia;
+  } else {
+    console.log(`No se encontró el equipo del piloto con id ${idPiloto}`);
+  }
+
+  let piloto = resultados.find((piloto) => piloto.idPiloto == idPiloto);
 
   if (piloto) {
     let carrera = {
@@ -84,12 +103,15 @@ export const editarNuevoResultado = (req, res) => {
       tecnicos,
       personales,
     };
-    
+
     // Buscar puntaje correspondiente a la posición
-    let puntaje = puntajes.find((p) => p.posicion === posicion);
+    let puntaje = puntajes.find((puntaje) => puntaje.posicion == posicion);
 
     if (puntaje) {
-      carrera.puntaje = puntaje.valor;
+      carrera.puntaje =
+        puntaje.valor !== undefined ? puntaje.valor : puntaje.puntaje;
+    } else {
+      console.log(`No se encontró el puntaje para la posición ${posicion}`);
     }
 
     if (!Array.isArray(piloto.carreras)) {
@@ -106,8 +128,8 @@ export const editarNuevoResultado = (req, res) => {
     // Si no se encuentra el piloto, crear uno nuevo
     piloto = {
       idPiloto,
-      escuderia,
       nombre,
+      escuderia,
       carreras: [],
     };
     resultados.push(piloto);
@@ -121,12 +143,14 @@ export const editarNuevoResultado = (req, res) => {
       personales,
     };
 
-     // Buscar puntaje correspondiente a la posición
-     const puntaje = puntajes.find((puntaje) => puntaje.posicion === posicion);
-     if (puntaje) {
-       carrera.puntaje = puntaje.puntaje;
-     }
-
+    // Buscar puntaje correspondiente a la posición
+    const puntaje = puntajes.find((puntaje) => puntaje.posicion == posicion);
+    if (puntaje) {
+      carrera.puntaje =
+        puntaje.valor !== undefined ? puntaje.valor : puntaje.puntaje;
+    } else {
+      console.log(`No se encontró el puntaje para la posición ${posicion}`);
+    }
     piloto.carreras.push(carrera);
 
     // guarda el array en un json
